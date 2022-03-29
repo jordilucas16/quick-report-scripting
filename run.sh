15eb453bac884308e168a4ee0ee4bac7c547daa8
Jordi Lucas <jordiluc16@gmail.com>
2022-03-29 18:25:39 +0200

    Rename d.sh to calculate-results.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ./pre-proccess-data.sh
 ./analyze-men-data.sh heart_men.csv
 gawk -f analyze-women-data.awk heart_women.csv >> results.csv
-./d.sh > results.html
+./calculate-results.sh > results.html
