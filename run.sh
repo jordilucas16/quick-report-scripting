8ea27405c032a45330cb9f26bfb716e4dd489ba5
Jordi Lucas <jordiluc16@gmail.com>
2022-03-29 18:23:54 +0200

    Rename files and update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./a.sh -v
-./b.sh
-./c_bash.sh heart_men.csv
-gawk -f c_awk.awk heart_women.csv >> results.csv
+./info.sh -v
+./pre-proccess-data.sh
+./analyze-men-data.sh heart_men.csv
+gawk -f analyze-women-data.awk heart_women.csv >> results.csv
 ./d.sh > results.html
